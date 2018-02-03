981d366bf7aaaf4544b2972fd4b6b99df31253f9
wozniaktomek <tomaszwozniak96@gmail.com>
2018-02-03 18:20:57 +0100

    added minimize function (temporary static)

---
+++ GeneticAlgorithm/src/pl/wozniaktomek/algorithm/Function.java
@@ -0,0 +1,7 @@
+package pl.wozniaktomek.algorithm;
+
+public class Function {
+    Double getResult(Double x, Double y) {
+        return 100 * Math.pow((Math.pow(x, 2.0) - y), 2.0) + Math.pow((1 - x), 2.0);
+    }
+}
